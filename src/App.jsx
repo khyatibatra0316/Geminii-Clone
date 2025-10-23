8ff5bf2367bd195ed2066e7f8ca833e51b9dab39
Khyati Batra <batrakhyati50@gmail.com>
2025-10-23 15:44:45 +0530

    hi

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Sidebar from './components/sidebar/sidebar.jsx'
+const App = () => {
+  return (
+    <>
+      <Sidebar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/sidebar/sidebar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './sidebar.css'
+
+const sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <div className='top'>
+            <img src='' alt="" />
+        </div>
+
+        <div className='bottom'>
+
+        </div>
+    </div>
+  
+  )
+}
+
+export default sidebar
