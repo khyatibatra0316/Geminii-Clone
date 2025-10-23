ee1eadbec04d278f1b0ca030afb21b61b87662f6
Khyati Batra <batrakhyati50@gmail.com>
2025-10-23 23:32:47 +0530

    fifth

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -10,7 +10,7 @@ const Main = () => {
         </div>
         <div className='main-container'>
             <div className='greet'>
-            <p><span>Hello,Dev.</span></p>
+            <p><span>Hello, Dev.</span></p>
             <p>How can I help you today?</p>
             </div>
             <div className="cards">
@@ -18,7 +18,31 @@ const Main = () => {
                     <p>Suggest beautiful places to see on an upcoming road trip</p>
                     <img src={assets.compass_icon} alt=""/>
                 </div>
+                <div className="card">
+                    <p>Briefly summarize this concept: urban planning</p>
+                    <img src={assets.bulb_icon} alt=""/>
+                </div>
+                <div className="card">
+                    <p>Brainstoem team bonding activities for our work retreat</p>
+                    <img src={assets.message_icon} alt=""/>
+                </div>
+                <div className="card">
+                    <p>Improve the readablitity of the following code</p>
+                    <img src={assets.code_icon} alt=""/>
+                </div>
             </div>
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type="text" placeholder='Enter a prompt here' />
+                    <div>
+                    <img src={assets.gallery_icon} alt="" />
+                    <img src={assets.mic_icon} alt="" />
+                    <img src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+                <p className="bottom-info">Gemini may diplay inaccurate info,including about people, so dounble-check its responses.</p>
+            </div>
+
         </div>
     </div>
   )
