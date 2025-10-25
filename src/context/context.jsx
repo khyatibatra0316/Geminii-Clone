f5975779b45a78851e71616c9012e383deee963e
Khyati Batra <batrakhyati50@gmail.com>
2025-10-25 13:30:30 +0530

    sixth

---
+++ src/context/context.jsx
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+
+export const context=createContext();
+
+const contextProvider=(props)=>{
+    
+
+
+    const contextValue={
+        
+    }
+    return(
+        <context.Provider value={contextValue}>
+            {props.children}
+        </context.Provider>
+    )
+}
